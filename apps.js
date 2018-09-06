3085d7b3ccb35f8beb19ea9013d99ef4a6ad024a
Jessica Duell <duellrdh@gmail.com>
2018-09-05 20:28:46 -0400

    updated

---
+++ apps.js
@@ -0,0 +1,8 @@
+$(document.ready, function () {
+
+    let top = document.querySelector(top);
+    let middle = document.querySelector(middle);
+    let bottom = document.querySelector(botton);
+    let right = document.querySelector(right);
+    let left = document.querySelector(left);
+})
