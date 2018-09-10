326119fdcf8820c101e35dfc86b815914d663b39
Jessica Duell <duellrdh@gmail.com>
2018-09-09 21:48:16 -0400

    updated players

--- apps.js
+++ apps.js
@@ -5,4 +5,10 @@ $(document.ready, function () {
     let bottom = document.querySelector(botton);
     let right = document.querySelector(right);
     let left = document.querySelector(left);
+    let center = document.querySelector(center);
+
+    let player1 = "X";
+    let player2 = "O";
+
+    
 })
